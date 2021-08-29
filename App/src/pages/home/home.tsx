ea0d2076ae0010cf274b9de4e2b44c48a3af6fa8
afi-arif <mdarif.afi@gmail.com>
2021-08-29 13:50:59 +0530

    advantage slide part done

--- App/src/pages/home/home.tsx
+++ App/src/pages/home/home.tsx
@@ -5,6 +5,10 @@ import smallboomerangLogo from "./../../assets/images/small/boomerang-logo.jpg";
 import mediumboomerangLogo from "./../../assets/images/medium/boomerang-logo.jpg";
 import largeboomerangLogo from "./../../assets/images/large/boomerang-logo.jpg";
 
+import iconGraph from "./../../assets/images/small/icon-graph.png";
+import iconHouse from "./../../assets/images/small/icon-house.png";
+import iconInverst from "./../../assets/images/small/icon-inverst.png";
+
 const Home: FC = () => {
     return (<main>
         <div className="boomerang-first-throw">
@@ -44,9 +48,19 @@ const Home: FC = () => {
             <h2>The Advantages of
                 Buy-to-Let Investment</h2>
             <div className="boomerang-home-slide">
-                <div>one</div>
-                <div>two</div>
-                <div>thr</div>
+                <div>
+                    <img src={iconGraph} />
+                    <h3>Asset Boost</h3>
+                    Owning a buy-to-let property can enable you to generate a regular monthly income, and benefit from an increase in the value of your asset over time too.
+                </div>
+                <div>
+                    <img src={iconHouse} />
+                    <h3>UK Property Market</h3>
+                    The UK lettings market is currently very strong and has been for a long time. This is due to a combination of factors including a lack of affordable properties to purchase (both residential and commercial), and strict mortgage underwriting criteria</div>
+                <div>
+                    <img src={iconInverst} />
+                    <h3>Safe Investment</h3>
+                    Both corporations and individuals that invest in buy-to-let property know that this is a stable, comparatively straight forward approach to achieving a consistent long-term return on their investment.</div>
             </div>
         </div>
     </main>)
