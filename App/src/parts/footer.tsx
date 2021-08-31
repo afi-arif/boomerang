d0527d96c9cb150de7bc8b20f0d6d1b6a378ed4d
afi-arif <mdarif.afi@gmail.com>
2021-08-31 09:55:36 +0530

    footer added

--- App/src/parts/footer.tsx
+++ App/src/parts/footer.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react";
+import "./../styles/footer.scss";
 
 const Footer: FC = () => {
-    return <footer>@copyrights Boomarang UK pvt ltd..</footer>
+    return (<footer>
+        <h2>Begin Your Journey
+            with Boomerang
+            Buy to Let Today</h2>
+        <p>Whatever your previous experience of buy-to-let investment, Boomerang Buy to Let can help you to create a profitable investment porftolio or take your existing porftolio to the next level.</p>
+        <p>Contact us now for a prompt, insightful response from an experienced buy-to-let porftolio manager, with no obligation.</p>
+    </footer>)
 }
 
 export default Footer;
