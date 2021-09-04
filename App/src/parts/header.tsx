91ebcf946696f89d3d801c6b556ceec0e3449e0f
afi-arif <mdarif.afi@gmail.com>
2021-09-04 13:15:40 +0530

    navi menu added

--- App/src/parts/header.tsx
+++ App/src/parts/header.tsx
@@ -9,17 +9,19 @@ const Header: FC = () => {
                 <a href="https://boomarang.co.uk">Home page</a>
                 <span>TANTUM PROJECTS</span>
             </div>
-            <nav className="boomerang-hand-holds">
+            <nav className="boomerang-hand-holds active">
                 <div className="menu-backscreen">
-                    <a className="menu" href="#">menu</a>
-                    <ul>
-                        <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/home">About us   </Link></li>
-                        <li><Link to="/home">What we do   </Link></li>
-                        <li><Link to="/projects">Projects   </Link></li>
-                        <li><Link to="/home">Blog</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
+                    <div className="navi-holder">
+                        <a className="menu" href="#">menu</a>
+                        <ul>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/home">About us   </Link></li>
+                            <li><Link to="/home">What we do   </Link></li>
+                            <li><Link to="/projects">Projects   </Link></li>
+                            <li><Link to="/home">Blog</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             <nav className="boomerang-desk-holds">
