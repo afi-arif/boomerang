f9a46bdf5e6b0d0da0082383f8f5d67e7e0a2e5f
afi-arif <mdarif.afi@gmail.com>
2021-09-08 09:33:02 +0530

    slide 1 done

--- App/src/pages/projects/slides/barkingRoad/barkingRoad.tsx
+++ App/src/pages/projects/slides/barkingRoad/barkingRoad.tsx
@@ -3,12 +3,112 @@ import "./barkingRoad.scss";
 
 const BarkingRoad: FC = () => {
     return (
-        <div className="project-slide">
-            <p>Our Barking Road project was originally a five-bedroom care home that we secured with the intention of attaining planning permission for both a change of use and of format, converting the building into a selection of residential apartments.</p>
-            <p>We applied for and were awarded permission to proceed with a three-storey rear extension to the property and redeveloped the newly-extended building into a total of five apartments.</p>
-            <p>These range from one to three bedrooms, with one of the one-bedroom apartments being particularly large and well-appointed for the area, targeting the premium lettings market and correlating hig her rental incomes.</p>
-            <p>As the site was initially zoned as a commercial premises, the deposit required from the client to secure the building was rather hig her than the norm at 30%; however, our development works have increased the rental value of the premises as a whole by an impressive 62%.</p>
-            <p>After completion of the relevant works and refinancing of the project in accordance with its increased value, our client’s net contribution to the project totalled just 52,000.</p>
+        <div className="boomerang-slider-barkingRoad">
+            <div className="project-info">
+                <p>Our Barking Road project was originally a five-bedroom care home that we secured with the intention of attaining planning permission for both a change of use and of format, converting the building into a selection of residential apartments.</p>
+                <p>We applied for and were awarded permission to proceed with a three-storey rear extension to the property and redeveloped the newly-extended building into a total of five apartments.</p>
+                <p>These range from one to three bedrooms, with one of the one-bedroom apartments being particularly large and well-appointed for the area, targeting the premium lettings market and correlating hig her rental incomes.</p>
+                <p>As the site was initially zoned as a commercial premises, the deposit required from the client to secure the building was rather hig her than the norm at 30%; however, our development works have increased the rental value of the premises as a whole by an impressive 62%.</p>
+                <p>After completion of the relevant works and refinancing of the project in accordance with its increased value, our client’s net contribution to the project totalled just 52,000.</p>
+            </div>
+            <div className="project-data">
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Loan</th>
+                            <th>Your Investment</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Purchase Price</td>
+                            <td>451.50</td>
+                            <td>261.30</td>
+                            <td>712.80</td>
+                        </tr>
+                        <tr>
+                            <td>Development Cost</td>
+                            <td>-</td>
+                            <td>355.30</td>
+                            <td>355.30</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>451.50</td>
+                            <td>616.60</td>
+                            <td>1,068.10</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Property Value Post Development</td>
+                            <td></td>
+                            <td></td>
+                            <td>1,300.00</td>
+                        </tr>
+                        <tr>
+                            <td>Profit</td>
+                            <td></td>
+                            <td></td>
+                            <td>231.90</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Re-finace @ 75% Post Development Value</td>
+                            <td></td>
+                            <td></td>
+                            <td>975.00</td>
+                        </tr>
+                        <tr>
+                            <td>Less: 1st Charge Loan</td>
+                            <td></td>
+                            <td></td>
+                            <td>- 451.50</td>
+                        </tr>
+                        <tr>
+                            <td>Less: Your Initial Investment</td>
+                            <td></td>
+                            <td></td>
+                            <td>- 616.60</td>
+                        </tr>
+                        <tr>
+                            <td>Client Contribution</td>
+                            <td></td>
+                            <td></td>
+                            <td>93.10</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Annual Rental Income</td>
+                            <td></td>
+                            <td></td>
+                            <td>58.20</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
